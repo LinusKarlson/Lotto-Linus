126e4a17f1d6b6275f26c67a1c6ee8f67f3beab3
Linus Karlsson <linus.karlsson@stud.prakticum.fi>
2018-09-11 02:23:50 -0700

    fixa så att det inte kommer duplikater, men inte optimerad

--- lotto-test-master/js/script.js
+++ lotto-test-master/js/script.js
@@ -1,10 +1,93 @@
 //slumptal mellan 1 och 39
-//OBS dålig kod, mjlighet till dublikat => använd inte ;)
+//OBS dålig kod,inte optimerad, men ger inte duplikat
+
+
 function lottoRad() {
+	
+	var checker=0;
+	var checker1=0;
+	var checker2=0;
+	var checker3=0;
+	var checker4=0;
+	var checker5=0;
+	var checker6=0;
+	
 	for (var i=0; i<=6; i++) {
 
 		let lottery = Math.floor(Math.random() * 38)+1;
-	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
-
+	 	
+		
+		
+		//console.log(checker);
+		
+	if (i==0){
+			 while ( lottery==checker1 || lottery==checker2 || lottery==checker3 || lottery==checker4 || lottery==checker5 || lottery==checker6){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+		} else if (i==1){
+			 while (lottery==checker ||  lottery==checker2 || lottery==checker3 || lottery==checker4 || lottery==checker5 || lottery==checker6){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+		}else if (i==2){
+			 while (lottery==checker || lottery==checker1 ||  lottery==checker3 || lottery==checker4 || lottery==checker5 || lottery==checker6){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+		}else if (i==3){
+			 while (lottery==checker || lottery==checker1 || lottery==checker2 ||  lottery==checker4 || lottery==checker5 || lottery==checker6){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+		}else if (i==4){
+			 while (lottery==checker || lottery==checker1 || lottery==checker2 || lottery==checker3 ||  lottery==checker5 || lottery==checker6){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+		}else if (i==5){
+			 while (lottery==checker || lottery==checker1 || lottery==checker2 || lottery==checker3 || lottery==checker4 ||  lottery==checker6){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+		}else{
+			 while (lottery==checker || lottery==checker1 || lottery==checker2 || lottery==checker3 || lottery==checker4 || lottery==checker5 ){
+			
+			 lottery = Math.floor(Math.random() * 38)+1;
+			//console.log(lottery);
+		}
+			//console.log(checker6);
+		}
+		
+		if (i==0){
+			 checker = lottery;
+		//console.log(checker);
+		} else if (i==1){
+			 checker1 = lottery;
+		}else if (i==2){
+			 checker2 = lottery;
+		}else if (i==3){
+			 checker3 = lottery;
+		}else if (i==4){
+			 checker4 = lottery;
+		}else if (i==5){
+			 checker5 = lottery;
+		}else{
+			 checker6 = lottery;
+			//console.log(checker6);
+		}
+		
+		console.log(checker);
+		document.getElementById ("lotto_" + i).innerHTML = lottery;
+		
 	}
+	
+	
+	
 }
